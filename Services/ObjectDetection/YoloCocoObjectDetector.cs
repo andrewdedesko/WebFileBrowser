7b1ed3bf5dfe5aa3e397a6014b22efc68747dfa1
Andrew Dedesko <adedesko@gmail.com>
2026-03-07 16:48:31 -0500

    Attempt to bundle yolo26s-coco.onnx again

--- Services/ObjectDetection/YoloCocoObjectDetector.cs
+++ Services/ObjectDetection/YoloCocoObjectDetector.cs
@@ -4,7 +4,7 @@ namespace WebFileBrowser.Services.ObjectDetection;
 
 public class YoloCocoObjectDetector : OnnxObjectDetector {
     public YoloCocoObjectDetector(ILogger<YoloCocoObjectDetector> logger) : base(
-        "Onnx/yolo26s-coco.onnx",
+        "onnx/yolo26s-coco.onnx",
         new Dictionary<int, LabelClass>{
             [0] = new LabelClass("person", DetectedObjectClass.Person),
             [13] = new LabelClass("bench", DetectedObjectClass.Unknown),
