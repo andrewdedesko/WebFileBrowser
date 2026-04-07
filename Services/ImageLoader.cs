b4ce553feeb4d8485ed03440cbedb1c616dc32d0
Andrew Dedesko <adedesko@gmail.com>
2026-04-07 10:34:22 -0400

    Cache image file hashes

--- Services/ImageLoader.cs
+++ Services/ImageLoader.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using Microsoft.Extensions.Caching.Distributed;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using WebFileBrowser.Models;
@@ -7,16 +8,22 @@ namespace WebFileBrowser.Services;
 
 public class ImageLoader {
     private readonly IShareService _shareService;
+    private readonly IDistributedCache _cache;
 
-    public ImageLoader(IShareService shareService) {
+    public ImageLoader(IShareService shareService, IDistributedCache cache) {
         _shareService = shareService;
+        _cache = cache;
     }
 
     public ImageWrapper Load(string share, string path) {
         var fsPath = _shareService.GetPath(share, path);
 
         var imageFileData = File.ReadAllBytes(fsPath);
-        var imageFileHash = SHA1.HashData(imageFileData);
+        byte[]? imageFileHash = _getCachedFileHash(share, path);
+        if(imageFileHash == null) {
+            imageFileHash = SHA1.HashData(imageFileData);
+            _cacheFileHash(share, path, imageFileHash);
+        }
 
         using(Stream stream = new MemoryStream(imageFileData)) {
             var image = Image.Load<Rgb24>(stream);
@@ -33,4 +40,14 @@ public class ImageLoader {
             return new ImageWrapper(share, path, imageFileHash, image);
         }
     }
+
+    private byte[]? _getCachedFileHash(string share, string path) {
+        return _cache.Get(_fileHashCacheKey(share, path));
+    }
+
+    private void _cacheFileHash(string share, string path, byte[] hash) {
+        _cache.Set(_fileHashCacheKey(share, path), hash);
+    }
+
+    private string _fileHashCacheKey(string share, string path) => $"FileHash:Sha1:{share}:{path}";
 }
