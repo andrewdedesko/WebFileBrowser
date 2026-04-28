aad4af35934aeb70912dcd1049565fff36d295cd
Andrew Dedesko <adedesko@gmail.com>
2026-04-27 22:59:18 -0400

    Add view models missed for explaining thumbnails

--- Models/ExplainThumbnailViewModel.cs
+++ Models/ExplainThumbnailViewModel.cs
@@ -4,3 +4,9 @@ public class ExplainThumbnailViewModel
 {
     public ImageThumbnailCandidateSet ImageThumbnailCandidateSet {get; set;}
 }
+
+public record ThumbnailOptionViewModel(string path);
+
+public record ThumbnailResultViewModel(string Share, string Path, IEnumerable<KeyValuePair<string, int>> PredictionCounts, ThumbnailCropResultViewModel? CropResult);
+
+public record ThumbnailCropResultViewModel(string CropMethod, double Score, int CropLeft, int CropTop, int CropRight, int CropBottom, IEnumerable<KeyValuePair<string, int>> PredictionCountsInCrop);
