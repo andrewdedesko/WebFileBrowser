4247d7b1874704478f61196ab58ea5f2627ad975
Andrew Dedesko <adedesko@gmail.com>
2026-04-27 22:54:56 -0400

    Add extension method DrawTextWithBackground

--- Extensions/SixLaborsImageExtensions.cs
+++ Extensions/SixLaborsImageExtensions.cs
@@ -20,6 +20,25 @@ public static class SixLaborsImageExtensions {
         image.Mutate(x => x.Resize(width, height));
     }
 
+    public static IImageProcessingContext DrawTextWithBackground(this IImageProcessingContext imageProcessingContext, string text, Color textColor, Color backgroundColor, float textX, float textY, float padding = 5f) {
+        var font = _getFont(14f);
+        FontRectangle size = TextMeasurer.MeasureBounds(text, new TextOptions(font));
+        var textLocation = new PointF(textX, textY);
+
+        var rect = new RectangleF(
+            textLocation.X - padding,
+            textLocation.Y - padding,
+            size.Width + (padding * 2),
+            size.Height + (padding * 2));
+
+        imageProcessingContext
+            .BackgroundColor(backgroundColor)
+            .Fill(backgroundColor, rect)
+            .DrawText(text, font, textColor, textLocation);
+
+        return imageProcessingContext;
+    }
+
     public static IImageProcessingContext DrawTextWithBackground(this IImageProcessingContext imageProcessingContext, string text, Font font, Color textColor, Color backgroundColor, float textX, float textY, float padding = 5f) {
         FontRectangle size = TextMeasurer.MeasureBounds(text, new TextOptions(font));
         var textLocation = new PointF(textX, textY);
@@ -40,5 +59,14 @@ public static class SixLaborsImageExtensions {
 
     public static Rectangle AsRectangle(this Box box) {
         return new Rectangle((int)Math.Floor(box.Left), (int)Math.Floor(box.Top), (int)Math.Floor(box.Width), (int)Math.Floor(box.Height));
+    }
+
+    public static Box AsBox(this Rectangle rectangle) =>
+        new Box(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom);
+
+    private static Font _getFont(float size) {
+        FontCollection fontCollection = new();
+        var family = fontCollection.Add("JetBrainsMono-Regular.ttf");
+        return family.CreateFont(size, FontStyle.Regular);
     }
 }
